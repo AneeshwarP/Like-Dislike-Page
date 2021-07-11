3247af8e2ad225568af47e78750c1382c71bde4a
Aneeshwar P <86723458+AneeshwarP@users.noreply.github.com>
2021-07-11 20:16:09 +0530

    Add files via upload

---
+++ like.js
@@ -0,0 +1,107 @@
+let likebtn = document.querySelector('#likebtn');
+let dislikebtn = document.querySelector('#dislikebtn');
+let likebtn2 = document.querySelector('#likebtn2');
+let dislikebtn2 = document.querySelector('#dislikebtn2');
+let likebtn3 = document.querySelector('#likebtn3');
+let dislikebtn3 = document.querySelector('#dislikebtn3');
+let likebtn4 = document.querySelector('#likebtn4');
+let dislikebtn4 = document.querySelector('#dislikebtn4');
+let likebtn5 = document.querySelector('#likebtn5');
+let dislikebtn5 = document.querySelector('#dislikebtn5');
+let likebtn6 = document.querySelector('#likebtn6');
+let dislikebtn6 = document.querySelector('#dislikebtn6');
+
+let input1 = document.querySelector('#input1');
+let input2 = document.querySelector('#input2');
+
+let input3 = document.querySelector('#input3');
+let input4 = document.querySelector('#input4');
+
+let input5 = document.querySelector('#input5');
+let input6 = document.querySelector('#input6');
+
+let input7 = document.querySelector('#input7');
+let input8 = document.querySelector('#input8');
+
+let input9 = document.querySelector('#input9');
+let input10 = document.querySelector('#input10');
+
+let input11 = document.querySelector('#input11');
+let input12 = document.querySelector('#input12');
+
+
+
+
+likebtn.addEventListener('click',()=>{
+    input1.value = parseInt(input1.value)+1;
+    input1.style.color = "#12ff00";
+   
+})
+
+dislikebtn.addEventListener('click',()=>{
+    input2.value = parseInt(input2.value)+1;
+    input2.style.color = "#ff0000";
+   
+})
+
+
+likebtn2.addEventListener('click',()=>{
+    input3.value = parseInt(input3.value)+1;
+    input3.style.color = "#12ff00";
+   
+})
+
+dislikebtn2.addEventListener('click',()=>{
+    input4.value = parseInt(input4.value)+1;
+    input4.style.color = "#ff0000";
+   
+})
+
+likebtn3.addEventListener('click',()=>{
+    input5.value = parseInt(input5.value)+1;
+    input5.style.color = "#12ff00";
+   
+})
+
+dislikebtn3.addEventListener('click',()=>{
+    input6.value = parseInt(input6.value)+1;
+    input6.style.color = "#12ff00";
+   
+})
+
+likebtn4.addEventListener('click',()=>{
+    input7.value = parseInt(input7.value)+1;
+    input7.style.color = "#12ff00";
+   
+})
+
+dislikebtn4.addEventListener('click',()=>{
+    input8.value = parseInt(input8.value)+1;
+    input8.style.color = "#12ff00";
+   
+})
+
+
+likebtn5.addEventListener('click',()=>{
+    input9.value = parseInt(input9.value)+1;
+    input9.style.color = "#12ff00";
+   
+})
+
+dislikebtn5.addEventListener('click',()=>{
+    input10.value = parseInt(input10.value)+1;
+    input10.style.color = "#12ff00";
+   
+})
+
+likebtn6.addEventListener('click',()=>{
+    input11.value = parseInt(input11.value)+1;
+    input11.style.color = "#12ff00";
+   
+})
+
+dislikebtn6.addEventListener('click',()=>{
+    input12.value = parseInt(input12.value)+1;
+    input12.style.color = "#12ff00";
+   
+})
